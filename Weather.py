fcc4381a770e4afb3ff40c24fa9ed3467c6f85e1
ankerovZubovichNikita <ankerov.n@mail.ru>
2021-10-19 00:42:14 +0600

    add file

--- Weather.py
+++ Weather.py
@@ -1,1 +1,18 @@
-print('weather')
+import requests  # Модуль для обработки URL
+from bs4 import BeautifulSoup  # Модуль для работы с HTML
+import time  # Модуль для остановки программы
+
+WeatherOmsk='https://www.gismeteo.ru/'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
+
+OmWeatherFullPage = requests.get(WeatherOmsk, headers=headers)  # Вывод полной разметки HTML страницы
+OmWeathersoup = BeautifulSoup(OmWeatherFullPage.content, 'html.parser')
+
+OmWeather=OmWeathersoup.find_all('span', {'value unit unit_temperature_c'})
+x=str(OmWeather[0])
+x1=x.split()
+y=str(x1[4])
+yred=y[:2]
+z=str(x1[5])
+zred=z[19:21]
+print('Температура в Омске на данный момент времени:', yred+zred,'градусов по Цельсию')
