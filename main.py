d37de9d35196afe172ecc36f996bc52b7f0aa7e4
VladislavGoroshko <vilyamsik@mail.ru>
2021-11-01 11:55:32 +0600

    Add file

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ EURO_RUB = 'https://www.cbr.ru/currency_base/daily/' #Ссылка на сайт
 TENGE_RUB = 'https://www.cbr.ru/currency_base/daily/' #Ссылка на сайт центробанка РФ где указаны все курсы валют
 STERLING_RUB = 'https://www.cbr.ru/currency_base/daily/' #Ссылка на сайт центробанка РФ где указаны все курсы валют
 GRIVEN_RUB = 'https://www.cbr.ru/currency_base/daily/' #Ссылка на сайт центробанка РФ где указаны все курсы валют
+
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'} #Заголовки для передачи вместе с URL
+
 Dollarfull_page = requests.get(DOLLAR_RUB, headers=headers) #Вывод полной разметки HTML страницы доллара
 Eurofull_page = requests.get(EURO_RUB, headers=headers) #Вывод полной разметки HTML страницы евро
 Tengefull_page = requests.get(TENGE_RUB,headers=headers) #Вывод полной разметки HTML страницы тенге
@@ -28,18 +30,23 @@ Dollarconvert = Dollarsoup.find_all("tr") #Находим нужный для н
 a=str(Dollarconvert[11]).split() # Разбиваем строку на элементы
 b=str(a[6]) # Выбираем 6 элемент строки
 t=b[4:11] # Делаем срез (остаются символы с 4 по 11)
+
 a1=str(Euroconvert[12]).split() # Разбиваем строку на элементы
 b1=str(a1[5]) # Выбираем 5 элемент строки
 t1=b1[4:11] # Делаем срез (остаются символы с 4 по 11)
+
 a2=str(Tengeconvert[14]).split() # Разбиваем строку на элементы
 b2=str(a2[6]) # Выбираем 6 элемент строки
 t2=b2[4:11] # Делаем срез (остаются символы с 4 по 11)
+
 a3=str(Sterlingconvert[29]).split() # Разбиваем строку на элементы
 b3=str(a3[8]) # Выбираем 8 элемент строки
 t3=b3[4:11] # Делаем срез (остаются символы с 4 по 11)
+
 a4=str(Grivenconvert[28]).split() # Разбиваем строку на элементы
 b4=str(a4[6]) # Выбираем 6 элемент строки
 t4=b4[4:11] # Делаем срез (остаются символы с 4 по 11)
+
 print("Курс одного доллара =" + " " + t + " " + "рублей") #Вывод курса валюты на экран
 print("Курс одного евро =" + " " + t1 + " " + "рублей") #Вывод курса валюты на экран
 print("Курс ста теньге =" + " " + t2 + " " + "рублей") #Вывод курса валюты на экран
