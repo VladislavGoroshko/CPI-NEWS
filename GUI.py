d1314f1536b57a3d21095c4982336960725cb21f
VladislavGoroshko <vilyamsik@mail.ru>
2021-11-09 16:56:41 +0600

    Add file

--- GUI.py
+++ GUI.py
@@ -4,17 +4,6 @@ from tkinter import messagebox
 root = Tk()
 
 
-def btn_click():
-    login = loginInput.get()
-    password = passField.get()
-
-    info_str= f'Данные: {str(login)}, {str(password)}'
-
-    messagebox.showinfo(title='Название', message=info_str)
-    #окно с ошибкой
-    #messagebox.showerror(title='', message='Ошибка')
-
-
 root.title('CPI-NEWS')
 root['bg'] = '#fafafa'
 root.wm_attributes('-alpha', 0.7)
@@ -30,16 +19,6 @@ frame.place(relwidth=1, relheight=1)
 
 title = Label(frame, text = "CPI-NEWS", bg = 'gray', font= 80)
 title.pack()
-btn= Button(frame, text = 'Введите ваш логин и пароль', bg = 'green', command=btn_click)
-btn.pack()
-
-
-
-loginInput = Entry(frame, bg ="grey")
-loginInput.pack()
-
-passField = Entry(frame, bg = "grey", show='*')
-passField.pack()
 
 
 
